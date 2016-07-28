cfeb5b6c95ce018e84bc12bd01ca81c51f4c9f23
Dmitry Ponyatov <dponyatov@gmail.com>
2016-07-28 22:22:07 +0400

    + p.46
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- pas.pas
+++ pas.pas
@@ -1,4 +1,26 @@
 program Expert;
+								// p.46
+Const
+
+WORD_MAX=40;					// max name length
+LINE_MAX=80;					// max textline length
+
+								// lexemes
+COLON	=':';
+PERIOD	='.'; 
+COMMA	=',';
+SPACE	=' ';
+EQUALS	='=';
+DEFINETE='100';
+
+Type
+								// user types
+word_string	=string[WORD_MAX];
+line_string	=string[LINE_MAX];
+
+value_ptr	=^val;//ue
+object_ptr	=^obj;//ect
+								// p.47
 
 begin
 	writeln('Hello World!');
